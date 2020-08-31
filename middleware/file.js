b3eb59c1cf4f7c4f80af5536fe7273a70f3b0ff0
Alexey Voloshin <voloshinalex1100@gmail.com>
2020-08-31 23:09:08 +0300

    add new prop

---
+++ middleware/file.js
@@ -0,0 +1,24 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+})
+
+const allowedTypes = ['image/png', 'image/jpeg', 'image/jpg'];
+
+const fileFilter = (req, file, cb) => {
+    if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+module.exports = multer({
+    storage, fileFilter
+})
